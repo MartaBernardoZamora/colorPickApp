872cd3bff61c444fe7b7151951de732e24897653
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-27 15:54:29 +0100

    one component estructure complete

--- src/components/FormColor.jsx
+++ src/components/FormColor.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 
 function FormColor() {
-    const [currentColor, setCurrentColor]=useState("#831B1B");
+    const [currentColor, setCurrentColor]=useState("#48fea9");
     function handleColorChange(e){
         setCurrentColor(e.target.value);
     }
     return (
         <>            
-            <div 
+            <div
                 className="colorSquare"
                 style={{backgroundColor: currentColor}}
-            >
+            >                
+                <p>Color seleccionado: {currentColor}</p>
             </div>
             <div className="form-color">
+                <h2>Selecciona un color</h2>
                 <input
                     onChange={handleColorChange} 
                     type="color" 
